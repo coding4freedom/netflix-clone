bfe22531eaaa34fe416c2a04665970d46dbad8c9
coding4freedom <chawkins2010@hotmail.com>
2024-09-30 20:55:25 -0500

    project folder structure updates, Navbar component complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
     <>
+      <Home />
     </>
   )
 }
 
-export default App
+export default App;
